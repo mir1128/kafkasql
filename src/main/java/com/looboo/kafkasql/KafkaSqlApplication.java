0095ac2befc80b1def9be33ed78aa0add82d1b8f
liujie <jieliu@thoughworks.com>
2019-06-07 15:21:32 +0800

    add lombok, but can not work now.

--- src/main/java/com/looboo/kafkasql/KafkaSqlApplication.java
+++ src/main/java/com/looboo/kafkasql/KafkaSqlApplication.java
@@ -1,7 +1,9 @@
 package com.looboo.kafkasql;
 
 import com.looboo.kafkasql.assemble.KafkaSqlDriver;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class KafkaSqlApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/looboo/kafkasql/assemble/KafkaSqlDriver.java
+++ src/main/java/com/looboo/kafkasql/assemble/KafkaSqlDriver.java
@@ -2,6 +2,7 @@ package com.looboo.kafkasql.assemble;
 
 import com.looboo.kafkasql.parser.KafkaSqlLexer;
 import com.looboo.kafkasql.parser.KafkaSqlParser;
+import lombok.extern.slf4j.Slf4j;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -9,11 +10,23 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
+@Slf4j
 public class KafkaSqlDriver {
+    private final String SELECT_TOKEN = KafkaSqlLexer.VOCABULARY.getDisplayName(KafkaSqlLexer.SELECT);
 
     public void parsing(String sql) {
         KafkaSqlParser parser = buildParser(sql.toUpperCase());
         ParseTree tree = parser.selectStatement();
+        if (tree.getChildCount() != 0 || !tree.getChild(0).getText().equals(SELECT_TOKEN)) {
+            log.warn("[syntax error] sql must start with select");
+            return;
+        }
+
+        processSubStatement(tree.getChild(1));
+    }
+
+    private void processSubStatement(ParseTree child) {
+
     }
 
     private KafkaSqlParser buildParser(String sql)  {
