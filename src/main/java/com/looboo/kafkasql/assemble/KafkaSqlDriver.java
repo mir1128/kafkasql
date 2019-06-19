6785b7b0d3ce47f60a98fe40eadd322a1bc12199
liujie <jieliu@thoughworks.com>
2019-06-19 21:09:56 +0800

    add syntax check.

--- src/main/java/com/looboo/kafkasql/assemble/KafkaSqlDriver.java
+++ src/main/java/com/looboo/kafkasql/assemble/KafkaSqlDriver.java
@@ -6,6 +6,7 @@ import com.looboo.kafkasql.parser.KafkaSqlParser;
 import lombok.extern.slf4j.Slf4j;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ErrorNodeImpl;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.io.ByteArrayInputStream;
@@ -25,7 +26,17 @@ public class KafkaSqlDriver {
         log.info("sql is {}", sql);
 
         KafkaSqlParser parser = buildParser(sql.toUpperCase());
-        ParseTree tree = parser.selectStatement();
+        ParseTree tree = null;
+        tree = parser.selectStatement();
+
+        if (hasErrorNode(tree)) {
+            return "syntax error.";
+        }
+
+        if (((KafkaSqlParser.SelectStatementContext)tree).exception != null) {
+            return "syntax error.";
+        }
+
         if (tree.getChildCount() == 0 || !tree.getChild(0).getText().equals(SELECT_TOKEN)) {
             log.warn("[syntax error] sql must start with select");
             return "";
@@ -36,10 +47,13 @@ public class KafkaSqlDriver {
 
     private String processSubStatement(ParseTree tree) {
         SelectProcessor selectProcessor = SelectProcessorFactory.createProcessor(tree, kafkaUtil);
+        if (selectProcessor == null) {
+            return "syntax error.";
+        }
         return selectProcessor.process(tree);
     }
 
-    private KafkaSqlParser buildParser(String sql)  {
+    private KafkaSqlParser buildParser(String sql) {
         ByteArrayInputStream inputStream = new ByteArrayInputStream(sql.getBytes());
         ANTLRInputStream input = null;
         try {
@@ -51,5 +65,15 @@ public class KafkaSqlDriver {
         KafkaSqlLexer lexer = new KafkaSqlLexer(input);
         CommonTokenStream tokenStream = new CommonTokenStream(lexer);
         return new KafkaSqlParser(tokenStream);
+    }
+
+    private boolean hasErrorNode(ParseTree tree) {
+        int childCount = tree.getChildCount();
+        for (int i = 0; i < childCount; ++i) {
+            if (tree.getChild(i) instanceof ErrorNodeImpl) {
+                return true;
+            }
+        }
+        return false;
     }
 }
