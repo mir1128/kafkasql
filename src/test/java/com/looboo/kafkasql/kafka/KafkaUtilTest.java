87090cd9e7b338955552bfd964568d3928036766
liujie <jieliu@thoughworks.com>
2019-06-20 11:14:02 +0800

    fix test.

--- src/test/java/com/looboo/kafkasql/kafka/KafkaUtilTest.java
+++ src/test/java/com/looboo/kafkasql/kafka/KafkaUtilTest.java
@@ -26,12 +26,14 @@ public class KafkaUtilTest extends KafkaTestBase {
 
         String topic1 = "test-topic-1";
         String topic2 = "test-topic-2";
+        String topic3 = "test-topic-3";
 
         Set<String> topics = StreamSupport.stream(kafkaUtil.listTopics().spliterator(), false).collect(Collectors.toSet());
 
         Assert.assertTrue(topics.size() == 3);
         Assert.assertTrue(topics.contains(topic1));
         Assert.assertTrue(topics.contains(topic2));
+        Assert.assertTrue(topics.contains(topic3));
 
     }
 
@@ -41,13 +43,15 @@ public class KafkaUtilTest extends KafkaTestBase {
         Properties properties = kafkaConsumerConfig.getConsumerProperties();
         properties.setProperty("group.id", "testgroup");
         Consumer consumer2 = new KafkaConsumer(properties);
+
         consumer1.subscribe(Arrays.asList("test-topic-2"));
         consumer2.subscribe(Arrays.asList("test-topic-2"));
+
         consumer1.poll(Duration.ofMillis(1000));
         consumer2.poll(Duration.ofMillis(1000));
 
         assertThat(kafkaUtil.listConsumerGroups().size(), is(2));
-        assertThat(kafkaUtil.listConsumerGroups(Arrays.asList("testgroup")).size(), is(1));
+        assertThat(kafkaUtil.listConsumerGroups(Arrays.asList("testgroup".toUpperCase())).size(), is(1));
 
         consumer1.close();
         consumer2.close();
